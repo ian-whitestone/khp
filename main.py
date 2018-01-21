f765818049e0ff1539739cf8d87d263b96c3f4cd
iwhitest <ianjameswhitestone@gmail.com>
2018-01-21 12:14:33 -0500

    Implement load to S3 pipeline
    * Get files already uploaded to S3 bucket
    * Get files in output dir
    * Load any files that have not already been loaded
    * Remove loaded files from output dir

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import logging
 FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 log = logging.getLogger(__name__)
+logging.getLogger("botocore").setLevel(logging.WARNING)
+logging.getLogger("boto3").setLevel(logging.WARNING)
 
-import boto3
+
 import os
 import config
+import utils
 from ImplicitlyTLS import tyFTP
+
 
 CONF = config.CONFIG
 
@@ -36,6 +40,28 @@ def download(folder, output_dir):
             with open(output_file, "wb") as f:
                 ftp.retrbinary("RETR " + filename, f.write, 8*1024)
 
+def load_to_s3(prefix=None):
+    """Load the downloaded files to S3, that have not already been uploaded.
 
-download('CSI_files', config.OUTPUT_DIR)
+    Args:
+        prefix (str, optional): Prefix of files to load. Defaults to None.
+    """
+    log.info("Loadings files with prefix {0} to S3".format(prefix))
+    s3_bucket = CONF['s3_bucket']
+    keys = utils.get_s3_keys(s3_bucket)
+    # get all files from output dir that is not in keys
+    files = utils.search_path(config.OUTPUT_DIR, prefix=prefix)
+    basenames = [os.path.basename(f) for f in files]
+    unloaded_files = [os.path.join(config.OUTPUT_DIR, f)
+                        for f in list(set(basenames) - set(keys))]
+
+    if len(unloaded_files) > 0:
+        utils.upload_to_s3(s3_bucket, unloaded_files)
+    else:
+        log.warning("No files to upload")
+
+    utils.clean_dir(config.OUTPUT_DIR, prefix=prefix)
+
+# download('CSI_files', config.OUTPUT_DIR)
 download('FTCI_files/Archive', config.OUTPUT_DIR)
+load_to_s3("V2")
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ Utils module, contains utility functions used throughout the codebase.
 import logging
 import yaml
 import os
+import boto3
 
 
 log = logging.getLogger(__name__)
@@ -31,3 +32,91 @@ def read_yaml(yaml_file):
     except Exception as e:
         log.error('Unable to read file %s. Error: %s' % (yaml_file,e))
         raise
+
+def upload_to_s3(s3_bucket, files):
+    """Upload a list of files to S3.
+
+    Args:
+        s3_bucket (str): Name of the S3 bucket.
+        files (list): List of files to upload
+    """
+    log.info("Attempting to load {0} files to s3 bucket: {1}".format(
+                len(files), s3_bucket))
+    s3 = boto3.resource('s3')
+    for f in files:
+        data = open(f, 'rb')
+        s3.Bucket(s3_bucket).put_object(Key=os.path.basename(f), Body=data)
+
+def get_s3_keys(s3_bucket):
+    """Get a list of keys in an S3 bucket.
+
+    Args:
+        s3_bucket (str): Name of the S3 bucket.
+
+    Returns:
+        keys (list): List of keys in the S3 bucket.
+    """
+    keys = []
+    s3 = boto3.client('s3')
+    resp = s3.list_objects_v2(Bucket=s3_bucket)
+    for obj in resp['Contents']:
+        keys.append(obj['Key'])
+    return keys
+
+
+def search_path(path, prefix=None, filetypes=[]):
+    """Search a path and return all the files. Optionally specify file prefixes
+    and/or filetypes to narrow your criteria.
+
+    Args:
+        path (str): input path
+        prefix (str, optional): File prefix
+        filetypes (list, optional): List of file types to search for
+            (i.e. ['.xls', '.xlsx', '.pdf'])
+
+    Returns:
+        files (list): list of files matching the specified filetypes
+    """
+    files = []
+    log.info('Searching for files in %s' % path)
+    for p in os.listdir(path):
+        fullPath = os.path.join(path, p)
+
+        type_check = True
+        prefix_check = True
+        if os.path.isfile(fullPath):
+            basename = os.path.basename(fullPath)
+            if filetypes:
+                type_check = fullPath.endswith(tuple(filetypes))
+            if prefix:
+                prefix_check = basename.startswith(prefix)
+            if type_check and prefix_check:
+                files.append(fullPath)
+    log.info("Found {0} files in {1} matching prefix '{2}' and filetypes {3}"
+                .format(len(files), path, prefix, filetypes))
+    return files
+
+def clean_dir(path, prefix=None):
+    """Helper function to clear any folders and files in a specified path.
+
+    Args:
+        path (str): input path
+        prefix (str, optional): File prefix
+    """
+    log.info("Cleaning folders in %s" % path)
+    prefix_check = True
+    for p in os.listdir(path):
+        fullPath = os.path.join(path, p)
+        if os.path.isdir(fullPath):
+            shutil.rmtree(fullPath)
+            assert not os.path.isdir(fullPath)
+            log.debug("Successfully removed folder " + fullPath)
+        elif os.path.isfile(fullPath):
+            basename = os.path.basename(fullPath)
+            if prefix is not None:
+                prefix_check = basename.startswith(prefix)
+            if prefix_check and basename.startswith('.') == False:
+                os.remove(fullPath)
+                assert not os.path.isfile(fullPath)
+                log.debug("Successfully removed file " + fullPath)
+    return
