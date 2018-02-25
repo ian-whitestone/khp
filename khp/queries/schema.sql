7c2a4d719af1f9c73e06764022c2e7aaa3be0620
iwhitest <ianjameswhitestone@gmail.com>
2018-02-24 21:34:21 -0500

    Add queue metadata table, enhanced transcripts fields

--- khp/queries/schema.sql
+++ khp/queries/schema.sql
@@ -50,8 +50,8 @@ CREATE TABLE contacts (
   secondary_agents TEXT,
   recording_identifier VARCHAR(15),
   contact_state INTEGER,
-  sender text,
-  receiver text,
+  sender TEXT,
+  receiver TEXT,
   contact_group_name VARCHAR(15),
   transcript_downloaded BOOLEAN,
   load_file VARCHAR(100),
@@ -61,10 +61,61 @@ CREATE TABLE contacts (
 
 CREATE TABLE transcripts (
   contact_id INTEGER,
-  sender text,
+  sender TEXT,
   display_name VARCHAR(100),
   dt TIMESTAMP,
   message_type INTEGER,
   message TEXT
 )
 ;
+
+
+CREATE TABLE enhanced_transcripts (
+  contact_id INTEGER,
+  handle_time INTEGER,
+  wait_time INTEGER,
+
+  khp_message_count INTEGER,
+  khp_double_message_count INTEGER, -- count times responded twice in a row
+  khp_mean_message_length DECIMAL(6,2),
+  khp_max_message_length INTEGER,
+  khp_mean_word_count DECIMAL(6,2),
+  khp_max_word_count INTEGER,
+  khp_sum_word_count INTEGER,
+
+  khp_mean_response_time DECIMAL(5,2), -- in seconds
+  khp_max_response_time DECIMAL(5,2),
+  khp_median_response_time DECIMAL(5,2),
+  khp_sum_response_time DECIMAL(5,2),
+
+
+  ext_message_count INTEGER,
+  ext_double_message_count INTEGER, -- count times responded twice in a row
+  ext_mean_message_length DECIMAL(6,2),
+  ext_max_message_length INTEGER,
+  ext_mean_word_count DECIMAL(6,2),
+  ext_max_word_count INTEGER,
+  ext_sum_word_count INTEGER,
+
+  ext_mean_response_time DECIMAL(5,2), -- in seconds
+  ext_max_response_time DECIMAL(5,2),
+  ext_median_response_time DECIMAL(5,2),
+  ext_sum_response_time DECIMAL(5,2),
+
+  -- TODO: add survey info!!
+
+)
+;
+
+CREATE TABLE queue_meta (
+  queue_id INTEGER,
+  lang_cd char(2),
+  platform varchar(20),
+  PRIMARY KEY (queue_id)
+)
+;
+
+INSERT INTO queue_meta VALUES (6020, 'EN', 'MOBILE');
+INSERT INTO queue_meta VALUES (6007, 'EN', 'DESKTOP');
+INSERT INTO queue_meta VALUES (6021, 'FR', 'MOBILE');
+INSERT INTO queue_meta VALUES (6008, 'FR', 'DESKTOP');
